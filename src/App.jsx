c699ed0be9b929b26d6c167ded3a677668d2928a
Shaharyar Ahmed <shah.s.a.ahmed@gmail.com>
2024-11-13 14:59:06 +0500

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-full h-screen bg-black text-white'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
