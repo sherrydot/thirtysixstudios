cf500f06f71ac05602658baf8c58d36f14cb56c2
Shaharyar Ahmed <shah.s.a.ahmed@gmail.com>
2024-11-22 15:32:33 +0500

    hero content

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,50 @@ function App() {
 
   return (
     <>
-      <div className="w-full min-h-screen">
-        {data[0].map((canvasdets, index) => (
+      <div className="w-full relative min-h-screen">
+        {/* {data[0].map((canvasdets, index) => (
           <Canvas key={index} details={canvasdets} />
-        ))}
+        ))} */}
+        <div className='w-full h-screen text-white'>
+          <nav className="top-0 left-0 w-full flex justify-between items-center px-20 py-4 z-50">
+            <div className="text-2xl font-bold">
+              thirtysixstudios.
+            </div>
+            <ul className="flex gap-8">
+              {['Home', 'About', 'Work', 'Contact'].map((link, index) => (
+                <li key={index} className="cursor-pointer hover:opacity-70 transition-opacity">
+                  {link}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="textcontainer px-[20%]">
+            <div className='text w-[40%]'>
+                <h3 className='text-4xl leading-[1.1]'>
+                  At Thirtysixstudio, we build immersive digital experiences for brands with a purpose.
+                </h3>
+                <p className='text-md w-[80%] mt-10 font-normal'>
+                  We’re a boutique production studio focused on design, motion, and creative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.
+                </p>
+                <p className='text-md mt-10 font-normal'>
+                  Scroll
+                </p>
+            </div>
+          </div>
+          <div className='w-full absolute bottom-0'>
+            <h1 className='text-[17rem] font-normal tracking-tight text-white pl-5'>
+              Thirtysixstudios.
+            </h1>
+          </div>
+        </div>
       </div>
-      <div className="w-full min-h-screen">
-        {data[1].map((canvasdets, index) => (
-          <Canvas key={index} details={canvasdets} />
-        ))}
-      </div>
-      <div className="w-full min-h-screen">
-        {data[2].map((canvasdets, index) => (
-          <Canvas key={index} details={canvasdets} />
-        ))}
+      <div className='w-full h-screen mt-32'>
+        <h1>
+          What is thirtysixstudios.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur nihil, hic repudiandae quam aspernatur aut saepe dolor dolores, corrupti nam, architecto delectus placeat blanditiis eius eum numquam magnam in illo minus adipisci quia reiciendis perferendis. Molestiae, dolores iusto architecto ea veniam blanditiis, consequatur tempora, officiis iste voluptatem exercitationem quae sint molestias! Nulla maxime, ex laudantium tempora excepturi, enim praesentium explicabo a illum ad eaque aliquid sed alias qui magni impedit. Sunt, fugit, optio debitis recusandae ipsa corporis velit porro totam maxime labore consequuntur accusamus quaerat, officia et suscipit possimus explicabo neque numquam? Nobis dignissimos iure impedit, et distinctio voluptas debitis?
+        </p>
       </div>
     </>
   );
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,68 +1,65 @@
-import PropTypes from 'prop-types'
-import { useEffect, useRef, useState } from "react"
-import canvasimages from "./canvasimages"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-
+import { useEffect, useRef, useState } from "react";
+import canvasImages from "./canvasimages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Canvas({ details }) {
   const { startIndex, numImages, duration, size, top, left, zIndex } = details;
 
-  const [index, setIndex] = useState({value: startIndex })
-  const canvasRef = useRef(null)
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
 
-  useGSAP(()=>{
+  useGSAP(() => {
     gsap.to(index, {
-      value: startIndex + numImages -1,
+      value: startIndex + numImages - 1,
       duration: duration,
       repeat: -1,
       ease: "linear",
       onUpdate: () => {
-        setIndex({ value: Math.round(index.value) })
-      }
-    })
-  })
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+
+    gsap.from(canvasRef.current, {
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
 
   useEffect(() => {
+    const scale = window.devicePixelRatio;
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d')
-    
-    const img = new Image()
-    img.src = canvasimages[index.value]
-    
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = canvasImages[index.value];
     img.onload = () => {
-      canvas.width = img.width
-      canvas.height = img.height
-      ctx.drawImage(img, 0, 0)
-    }
-  }, [index])
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
 
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    };
+  }, [index]);
 
   return (
     <canvas
       data-scroll
-      data-scroll-speed={
-        Math.random().toFixed(1)
-      }
-      id='canvas' 
-      ref={canvasRef} 
-      className='relative'
+      data-scroll-speed={Math.random().toFixed(1)}
+      ref={canvasRef}
+      className="absolute"
       style={{
-        width: `${size*1.3}px`,
-        height: `${size*1.3}px`,
+        width: `${size * 1.8}px`,
+        height: `${size * 1.8}px`,
         top: `${top}%`,
         left: `${left}%`,
-        zIndex: `${zIndex}`
+        zIndex: `${zIndex}`,
       }}
-    >
-    </canvas>
-  )
+      id="canvas"
+    ></canvas>
+  );
 }
 
-Canvas.propTypes = {
-  details: PropTypes.shape({
-    startIndex: PropTypes.number.isRequired
-  }).isRequired
-}
-
-export default Canvas
+export default Canvas;
